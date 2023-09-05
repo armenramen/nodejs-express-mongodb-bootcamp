16fc1838944ed9f399c9f34179f1e117b8041888
armenramen <delacruzrmn@gmail.com>
2023-09-05 13:22:20 +0800

    Listeners with http requests

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 
 // using class
@@ -22,3 +23,27 @@ myEmitter.on('newSale', stock => {
 });
 
 myEmitter.emit('newSale', 9);
+
+
+
+/////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log('Request received');
+    res.end('Request received')
+});
+
+server.on('request', (req, res) => {
+   console.log('Another request ❤️');
+});
+
+server.on('close', () => {
+    console.log('Server close');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Waiting for requests...');
+});
+
